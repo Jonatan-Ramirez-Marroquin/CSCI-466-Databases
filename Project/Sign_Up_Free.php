4a4bee63af46d5dcb26bd6a631e169336517ecf5
Jonatan-Ramirez-Marroquin <155495901+Jonatan-Ramirez-Marroquin@users.noreply.github.com>
2024-01-03 19:53:42 -0600

    Update Sign_Up_Free.php

--- Project/Sign_Up_Free.php
+++ Project/Sign_Up_Free.php
@@ -3,7 +3,7 @@
 	include("personal_Login.php");
 	try{
 		//Provide information on specific Artist.
-		$dsn = "mysql:host=courses;dbname=z1912344";
+		$dsn = "mysql:host=courses;dbname=mydatabase";
 		$pdo = new PDO($dsn, $userName, $password);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
@@ -56,7 +56,7 @@
 		Send user back to Home Page
 		-->
 		<h2>Return to Home</h2>
-		<form action = "http://students.cs.niu.edu/~z1912344/htmlPage_Project.php" method = "GET">
+		<form action = "http://students.cs.niu.edu/~mydatabase/htmlPage_Project.php" method = "GET">
 			<input type = "Submit" name = "Submit" value = "Submit"/>	
 		</form>
 		<br>
